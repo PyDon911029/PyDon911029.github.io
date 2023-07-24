752d8d1269a814c6b7d61fcfb951ae397d6d7976
PyDon911029 <jym911029@gmail.com>
2023-07-24 17:49:32 +0900

    js update

--- contant.js
+++ contant.js
@@ -1,2 +1,376 @@
+var user_name = "Quan Ming";
+var designer = "JYM";
+var designer_url = "#";
+var cv_filename = "cv_me.docx";
+var profile_title = "Senior Full-Stack Developer";
+var header_banner = "";
+var footer_banner = "";
+
+// about
 var about_title = "Hello! I am Quan Ming. Web Developer, Mobile Developer and Blockchain & AI developer.";
-var about_content = "I am a project manager & senior Web Developer with over 9+ years of experience in web and Mobile and Blockchain including agency work, e-commerce, publishing, and sports betting.<br/> Client satisfaction is my topmost priority and this is the hallmark of our success.<br/> Thanks.<br>Regards";
+var about_content = "I am a project manager & senior Web Developer with over 9+ years of experience in web \
+                    and Mobile and Blockchain including agency work, e-commerce, publishing, and sports betting.\
+                    <br/> Client satisfaction is my topmost priority and this is the hallmark of our success.<br> Thanks.<br>Regards";
+
+// contact info
+var contact_info = [
+    {
+        key: "facebook",
+        url: "#",
+        title: "Follow me on Facebook",
+        visible: true
+    },
+    {
+        key: "whatsapp",
+        url: "#",
+        title: "Follow me on WhatsApp",
+        visible: true
+    },
+    {
+        key: "telegram",
+        url: "https://t.me/jym911029",
+        title: "Follow me on Telegram",
+        visible: true
+    },
+    {
+        key: "skype",
+        url: "skype:live:.cid.e76a7a10c0592521?chat",
+        title: "Follow me on Skype",
+        visible: true
+    }
+];
+
+// basic information
+var basic_info = {
+    title: "Basic Information",
+    content: [
+        {
+            title: "Email",
+            content: "jym911029@gmail.com",
+            visible: true
+        },
+        {
+            title: "Phone",
+            content: "+856 51 51 3028",
+            visible: true
+        },
+        {
+            title: "Skype",
+            content: "live:.cid.e76a7a10c0592521",
+            visible: true
+        },
+        {
+            title: "Address",
+            content: "Hui Zhong Li 309lou 131shi, City Area - Chaoyang District, Beijing, China",
+            visible: true
+        },
+        {
+            title: "Language",
+            content: "English, Japanese, Arabic",
+            visible: true
+        }
+    ]
+};
+
+// skill set
+var skill_set = [
+    {
+        title: "Programming Languages",
+        visible: true,
+        content: [
+            {
+                title: "Python",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "PHP",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "C",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Java",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Perl",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Dart",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Go",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "R",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Kotlin",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Lisp",
+                percent: 90,
+                visible: true
+            }
+        ]
+    },
+    {
+        title: "Web & Mobile development",
+        visible: true,
+        content: [
+            {
+                title: "HTML5",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "CSS",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "JavaScript",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "SASS/LESS",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Bootstrap",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Tailwind CSS",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "UI/UX",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Laravel",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "CodeIgniter",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Django",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "MERN Stack",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Next.js",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Nest.js",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Ruby on Rails",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "ASP.net",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Android Studio",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "React native",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Flutter",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Ionic",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Cordova",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Swift",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "iPhone/iOs",
+                percent: 90,
+                visible: true
+            }
+        ]
+    },
+    {
+        title: "Others",
+        visible: true,
+        content: [
+            {
+                title: "Blockchain",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Bitcoin",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Ethereum",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Smart Contract",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "NFT",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "DAPP",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "DeFi",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Solana",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Solidity",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Artificial Intelligence",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Image Processing",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Data Processing",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "NLP",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Web Scraping",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "e-Commerce",
+                percent: 90,
+                visible: true
+            }
+        ]
+    },
+    {
+        title: "Database Management",
+        visible: true,
+        content: [
+            {
+                title: "MySQL",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Postgres SQL",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "SQlite",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "MongoDB",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Redis",
+                percent: 90,
+                visible: true
+            },
+            {
+                title: "Oracle",
+                percent: 90,
+                visible: true
+            }
+        ]
+    }
+];
+
+// portfolio
+
+// work experience
+
+// educaiton
+
+// reference
+
+// contact me
--- data_manager.js
+++ data_manager.js
@@ -1,2 +1,108 @@
+document.title = user_name + "Co.Lt";
+document.getElementById("logo-title").innerHTML = user_name + "Co.Lt";
+document.getElementById("header-title").innerHTML = user_name;
+document.getElementById("profile-title").innerHTML = profile_title;
+
+// header contact list
+for (let i = 0; i < contact_info.length; i++) {
+    var temp = document.getElementById("header-contact-list").innerHTML;
+    if(contact_info[i].visible)
+    document.getElementById("header-contact-list").innerHTML = temp 
+        + "<a class='btn btn-default btn-round btn-lg btn-icon' href='" 
+        + contact_info[i].url 
+        + "' rel='tooltip' title='" 
+        + contact_info[i].title 
+        + "'><i class='fa fa-" 
+        + contact_info[i].key + "'></i></a>";
+}
+
+// footer contact list
+for (let i = 0; i < contact_info.length; i++) {
+    var temp = document.getElementById("footer-contact-list").innerHTML;
+    if(contact_info[i].visible)
+    document.getElementById("footer-contact-list").innerHTML = temp 
+        + "<a class='cc-" 
+        + contact_info[i].key 
+        + " btn btn-link' href='" 
+        + contact_info[i].url 
+        + "'><i class='fa fa-" 
+        + contact_info[i].key 
+        + " fa-2x ' aria-hidden='true'></i></a>";
+}
+
+// about
 document.getElementById("about-title").innerHTML = about_title;
-document.getElementById("about-content").innerHTML = about_content
+document.getElementById("about-content").innerHTML = about_content;
+
+// basic information
+document.getElementById("basic-info").innerHTML = "<div class='h4 mt-0 title'>" + basic_info.title + "</div>";
+for (let i = 0; i < basic_info.content.length; i++) {
+    var temp = document.getElementById("basic-info").innerHTML;
+    if(basic_info.content[i].visible)
+    document.getElementById("basic-info").innerHTML = temp 
+        + "<div class='row mt-3'><div class='col-sm-4'><strong class='text-uppercase'>" 
+        + basic_info.content[i].title 
+        + ":</strong></div><div class='col-sm-8'>" 
+        + basic_info.content[i].content 
+        + "</div></div>";
+}
+
+// skill set
+for (let i = 0; i < skill_set.length; i++) {
+    var skill_set_tmp = document.getElementById("skill-set").innerHTML;
+    if (skill_set[i].visible) {
+        skill_set_tmp = skill_set_tmp 
+            + "<div class='text-left mb-4 title'>" 
+            + skill_set[i].title 
+            + "</div>";
+        skill_set_tmp = skill_set_tmp 
+            +   "<div class='card' data-aos='fade-up' data-aos-anchor-placement='top-bottom'>\
+                    <div class='card-body'>\
+                        <div class='row'>";
+        for (let j = 0; j < skill_set[i].content.length; j++) {
+            if (skill_set[i].content[j].visible) {
+                skill_set_tmp = skill_set_tmp 
+                    +   "<div class='col-md-3'>\
+                            <div class='progress-container progress-primary'>\
+                                <span class='progress-badge'>" + skill_set[i].content[j].title + "</span>\
+                                <div class='progress'>\
+                                    <div class='progress-bar progress-bar-primary' \
+                                        data-aos='progress-full' \
+                                        data-aos-offset='10' \
+                                        data-aos-duration='2000' \
+                                        role='progressbar' \
+                                        aria-valuenow='" + skill_set[i].content[j].percent + "' \
+                                        aria-valuemin='0' \
+                                        aria-valuemax='100' \
+                                        style='width: 80%;'>\
+                                    </div>\
+                                    <span class='progress-value'>80%</span>\
+                                </div>\
+                            </div>\
+                        </div>"
+            }
+        }
+        skill_set_tmp = skill_set_tmp 
+            + "<div/><div/><div/>";
+        document.getElementById("skill-set").innerHTML = skill_set_tmp;
+    }
+    
+}
+// portfolio
+
+// work experience
+
+// education
+
+// reference
+
+// contact me
+
+// footer info
+document.getElementById("copyright").innerHTML = "&copy; " 
+    + user_name 
+    + " CV. All rights reserved.<br>Design - <a id='designer' class='credit' href='" 
+    + designer_url 
+    + "' target='_blank'>" 
+    + designer 
+    + "</a>";
